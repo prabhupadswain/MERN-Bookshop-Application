7bb7f2715e6a4107cdcd013f94360a0b6b7dd178
prabhupadswain <prabhupad.swain@outlook.com>
2024-03-21 17:34:40 +0530

    Database connection successful

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 // Import environment file configurations
 const dotenv = require('dotenv').config();
 
 // Create an express app
 const app = express();
+
+// Database Connection
+connectDB();
 
 // Middleware handling requests
 app.use('/', (req, res) => res.send('OK from server'));
